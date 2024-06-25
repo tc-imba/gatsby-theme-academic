91d6c8fb73cd83011aee828c89a3f81f0732c43a
tc-imba <liuyh970615@gmail.com>
2024-06-26 03:32:21 +0800

    fix: cache problem (attempt 4)

--- gatsby-theme-academic/gatsby-browser.js
+++ gatsby-theme-academic/gatsby-browser.js
@@ -5,6 +5,11 @@ import {
 export const wrapPageElement = _wrapPageElement;
 
 // trigger an immediate page refresh when an update is found
+export const onServiceWorkerUpdateFound = () => {
+  console.log('update found, reload the page');
+  window.location.reload();
+};
+
 export const onServiceWorkerUpdateReady = () => {
   console.log('update found, reload the page');
   window.location.reload();
